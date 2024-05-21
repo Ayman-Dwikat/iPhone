499e5daf6567884c765083baf61a9053f815f818
ayman dwikat <ayman.dwikat.2001@gmail.com>
2024-05-21 08:36:39 +0300

    Upload The Project

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+
+function Phones(Phone_src) {
+  img.src = Phone_src;
+}
+
+function colors(background_color) {
+  container.style.background = background_color;
+}
